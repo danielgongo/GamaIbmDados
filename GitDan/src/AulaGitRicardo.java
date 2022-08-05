e429492bfa705b16a6910574219e67135d327f6e
Daniel Gongo <dan_liveira@outlook.com>
2022-08-05 10:23:57 -0300

    Atualização

--- GitDan/src/AulaGitRicardo.java
+++ GitDan/src/AulaGitRicardo.java
@@ -3,6 +3,8 @@ public class AulaGitRicardo {
 
 	public static void main(String[] args) {
 		// Teste git professor Ricardo
+		
+		System.out.println("Fazer uma atualização do git hub aula 10");
 
 	}
 
